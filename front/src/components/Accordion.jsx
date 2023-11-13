2f68b0ac8692be77a295eb56c0db8e41281ebfc0
YounusMuradi <younusmuradi2023@gmail.com>
2023-11-13 09:41:38 +0430

    nochanges

--- front/src/components/Accordion.jsx
+++ front/src/components/Accordion.jsx
@@ -15,7 +15,7 @@ const Accordion = () => {
         </div>
 
         <div className="-mx-4 flex flex-wrap">
-          <div className="w-full px-4  lg:w-1/2">
+          <div className="w-full px-4   lg:w-1/2">
             <AccordionItem
               header="How long we deliver your first blog post?"
               text="It takes 2-3 weeks to get your first blog post ready. That includes the in-depth research & creation of your monthly content marketing strategy that we do before writing your first blog post, Ipsum available ."
